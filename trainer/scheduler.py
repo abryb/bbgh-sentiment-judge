947473709d48c9cc39935b02f4be803f1707bef3
Błażej Rybarkiewicz <b.rybarkiewicz@gmail.com>
2021-02-12 13:10:33 +0100

    Scheduler wating for internet connection

--- trainer/scheduler.py
+++ trainer/scheduler.py
@@ -1,9 +1,32 @@
 import schedule
 import subprocess
 import time
+import socket
 
 if __name__ == '__main__':
+    def has_internet_connections(host="8.8.8.8", port=53, timeout=3):
+        """
+        Host: 8.8.8.8 (google-public-dns-a.google.com)
+        OpenPort: 53/tcp
+        Service: domain (DNS/TCP)
+        """
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            return True
+        except socket.error as ex:
+            return False
+
     def run():
+        if not has_internet_connections():
+            print("Waiting for internet connection... Checking interval: 10s Max waiting time: 2 hours.")
+            slept = 0
+            while not has_internet_connections():
+                time.sleep(10)
+                slept += 10
+                if slept > 3600 * 2:
+                    raise Exception("No internet connection")
+
         subprocess.call("python -m trainer run", shell=True)
 
     run()
